3ff3acf7ae15b8294ab6f2febe833096c11239a3
samedyolchuyev@outlook.com <samedyolchuyev@outlook.com>
2024-04-03 23:47:51 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(502, 316);
+            Name = "Form1";
+            Text = "Form1";
+            MouseDown += MainForm_MouseDown;
+            MouseMove += MainForm_MouseMove;
+            MouseUp += MainForm_MouseUp;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Button currentButton;
+        private int buttonCount = 1;
+        private Point startPoint;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                currentButton = new Button();
+                currentButton.Width = 0;
+                currentButton.Height = 0;
+                currentButton.FlatStyle = FlatStyle.Flat;
+                currentButton.BackColor = Color.Cyan;
+                this.Controls.Add(currentButton);
+                currentButton.BringToFront();
+                startPoint = e.Location;
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (currentButton != null && e.Button == MouseButtons.Left)
+            {
+                int width = Math.Abs(e.X - startPoint.X);
+                int height = Math.Abs(e.Y - startPoint.Y);
+                currentButton.Location = new Point(Math.Min(startPoint.X, e.X), Math.Min(startPoint.Y, e.Y));
+                currentButton.Size = new Size(width, height);
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (currentButton != null)
+            {
+                if (currentButton.Width > 0 && currentButton.Height > 0)
+                {
+                    currentButton.Text = buttonCount.ToString();
+                    buttonCount++;
+                }
+                else
+                {
+                    this.Controls.Remove(currentButton);
+                }
+                currentButton = null;
+            }
+        }
+    }
+}
